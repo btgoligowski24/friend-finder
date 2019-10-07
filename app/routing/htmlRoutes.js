68708b88c1e8baf1a6803e171d01e2a336fdd25f
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-10-07 09:13:58 -0500

    updating html routes to dynamically send to home page

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,11 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../../app/public/survey.html"));
   });
 
-  app.get(/.*/, function (req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../../app/public/home.html"));
+  });
+
+  app.get(/.*/, function (req, res) {
+    res.redirect("/");
   });
 }
