63603a26b63ed37e1e0089a9cf4e0db8eaa79c69
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-10-06 19:15:12 -0500

    added html validation and updated routes to match user, now on to testing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,34 @@
 let friendsFile = require("../data/friends.js");
+let friends = friendsFile.friends;
 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
-        return res.json(friendsFile.friends);
+        return res.json(friends);
     });
-    
+
     app.post("/api/friends", function (req, res) {
         // req.body hosts is equal to the JSON post sent from the user
         // This works because of our body parsing middleware
-        let newFriend = req.body;
+        const newFriend = req.body;
         let matchedFriend;
-    
+
         console.log(newFriend);
-    
-        friendsFile.friends.push(newFriend);
-    
+
+        for (obj of friends) {
+            let sumDifference = 0;
+            for (val of friends[obj].scores) {
+                sumDifference += Math.abs(friends[obj].scores[val] - newFriend.scores[val]);
+            }
+            if (!matchedFriend) {
+                matchedFriend = friends[obj];
+                matchedFriend.difference = sumDifference;
+            } else if (matchedFriend.difference > sumDifference) {
+                matchedFriend = friends[obj];
+                matchedFriend.difference = sumDifference;
+            }
+        }
+        friends.push(newFriend);
+
         // This is what is sent back to the post callback on the reserve.html page. This essentially becomes the "data" parameter in that call back.
         res.json(matchedFriend);
     });
